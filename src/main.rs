9375e91851a4dab48e38db0e29860cca73d354d3
u-na-gi <orcinusorca1758dv6932@gmail.com>
2024-03-06 11:02:39 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+// extern crate hyper;
+// extern crate hyper_rustls;
+extern crate google_pubsub1 as pubsub1;
+use pubsub1::{Result, Error};
+use std::default::Default;
+use pubsub1::{Pubsub, oauth2, hyper, hyper_rustls, chrono, FieldMask};
+
+
+
+
+#[tokio::main]
+async fn main() {
+    // Get an ApplicationSecret instance by some means. It contains the `client_id` and 
+// `client_secret`, among other things.
+let secret: oauth2::ApplicationSecret = Default::default();
+// Instantiate the authenticator. It will choose a suitable authentication flow for you, 
+// unless you replace  `None` with the desired Flow.
+// Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about 
+// what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
+// retrieve them from storage.
+let auth = oauth2::InstalledFlowAuthenticator::builder(
+        secret,
+        oauth2::InstalledFlowReturnMethod::HTTPRedirect,
+    ).build().await.unwrap();
+let mut hub = Pubsub::new(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().build()), auth);
+// You can configure optional parameters by calling the respective setters at will, and
+// execute the final call using `doit()`.
+// Values shown here are possibly random and not representative !
+let result = hub.projects().schemas_get_iam_policy("resource")
+             .options_requested_policy_version(-33)
+             .doit().await;
+
+match result {
+    Err(e) => match e {
+        // The Error enum provides details about what exactly happened.
+        // You can also just use its `Debug`, `Display` or `Error` traits
+         Error::HttpError(_)
+        |Error::Io(_)
+        |Error::MissingAPIKey
+        |Error::MissingToken(_)
+        |Error::Cancelled
+        |Error::UploadSizeLimitExceeded(_, _)
+        |Error::Failure(_)
+        |Error::BadRequest(_)
+        |Error::FieldClash(_)
+        |Error::JsonDecodeError(_, _) => println!("{}", e),
+    },
+    Ok(res) => println!("Success: {:?}", res),
+}
 }
